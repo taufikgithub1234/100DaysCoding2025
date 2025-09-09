b9b9ddb1f317475dcf4731de17aa81ba4d8a6d4e
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-09-10 06:58:26 +0800

    Create days2.java

---
+++ days2.java
@@ -0,0 +1,16 @@
+public class main{
+  public class static void main(String[] args){
+    // Komentar untuk satu baris
+    
+    /*
+    komentar untuk beberapa kalimat,seperti
+    kalimat pertama,
+    kalimat kedua,
+    dst...
+    */
+    
+    /*
+    sama halnya diatas komentar untuk beberapa kalimat.
+    **/
+  }
+}
