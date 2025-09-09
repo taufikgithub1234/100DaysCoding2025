5b0f1ea48d2e05a8703dcad5a65fadb7976c22d1
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-09-09 13:33:04 +0800

    Create Days2.java

---
+++ Days2.java
@@ -0,0 +1,9 @@
+public class main {
+public class static void main(String[] args) {
+
+System.out.println("Contoh Println");
+System.out.print("Contoh Print");
+System.out.printf("Nama saya %s/n Kelas saya %s/n umur %d","Taufik","Informatika A",19);
+}
+
+}
