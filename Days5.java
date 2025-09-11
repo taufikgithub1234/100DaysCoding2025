5cf7e9c2941a4dbed00efbf73cbb6aa645513640
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-09-12 05:47:06 +0800

    Create Days5.java

---
+++ Days5.java
@@ -0,0 +1,13 @@
+public class numerik {
+    public static void main(String[] args) {
+        byte umur = 19;
+        short JumlahMhs = 3000;
+        int gajiBulanan = 2000000;
+        long jarak = 149_600_000_000l;
+        System.out.println("Umur saya sekarang "+umur+" tahun");
+        System.out.println("Jumlah Mahasiswa : "+JumlahMhs+" orang");
+        System.out.println("Gaji Bulanan: Rp "+gajiBulanan);
+        System.out.println("Jarak ku dengan dia sejauh: "+jarak+" meter");        
+        
+    }
+}
