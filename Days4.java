6066643b53c5f27a137c4e0004fdccd3f4fa2d8c
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-09-11 21:29:56 +0800

    Create Days4.java

---
+++ Days4.java
@@ -0,0 +1,12 @@
+public class Escape {
+    public static void main(String[] args) {
+        System.out.println("Halo \nDunia");
+        System.out.println("Nama:\tTaufik");
+        System.out.println("Umur:\t19");
+        System.out.println("Dia berkata: \"Halo Senior!\"");
+        System.out.println("Assalamu'alaikum");
+        System.out.println("Folder ada di C:\\Users\\Taufik");
+        System.out.println("Halo\rDunia");
+        System.out.println("Helloo\b!");
+    }
+}
