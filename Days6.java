025fb9489899c1c021e96383851d7be5100d1d33
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-09-13 19:25:07 +0800

    Create Days6.java

---
+++ Days6.java
@@ -0,0 +1,8 @@
+public class tipedeta {
+  public static void main(String[] args) {
+float tinggi = 165.5f;
+double lingkaran = 3.14;
+    System.out.println("Tinggi saya adalah: "+tinggi+" cm");
+    System.out.println("phi pada lingkaran adalah: "+lingkaran;
+  }
+}
