56b462578c7ef1ef499e34eeaf91b3c7ae7f6a29
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-09-14 16:39:07 +0800

    Create Days7.java

---
+++ Days7.java
@@ -0,0 +1,7 @@
+public class tipedata{
+  public static void main(String[] args) {
+    char nilai = 'A';
+    System.out.println("Predikat nilai saya adalah: "+nilai);
+    System.out.println("Nilai 89 lebih besar dari 77:"+ (89>77));
+  }
+}
