ea9d465a37f23ad6ba076832e2c97c0fc0bc8f1a
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-09-16 23:44:24 +0800

    Create Days9.java

---
+++ Days9.java
@@ -0,0 +1,16 @@
+publiic class Days9 {
+  public static void main(String[] args){
+    // untuk deklarasi
+    int umur;      
+    String nama;    
+    // untuk inisialisasi
+    int umur = 18;          
+    String nama = "Andi";  
+    // update
+    int umur = 18;    
+    System.out.println("umur sebelum update: " + umur);
+    umur = 19;         
+    System.out.println("umur setelah update: " + umur);
+
+  }
+}
