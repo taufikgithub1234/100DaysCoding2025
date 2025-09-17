6ceb17aad2b6e9380690bf04844b70f313bd92ab
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-09-17 22:46:41 +0800

    Create Days10.java

---
+++ Days10.java
@@ -0,0 +1,8 @@
+public class constanta{
+  public static void main(String[] args) {
+    final int hidung = 2;
+    final int mobil = 3;
+    System.out.println("Jumlah lubang hidung: "+hidung;
+    System.out.print(" Jumlah mobil ku miliki: "+mobil;
+  }
+}
