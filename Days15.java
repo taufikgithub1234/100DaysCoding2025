625d2ecf98be263dc58c97dd8d5ac91981981390
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-09-22 21:15:42 +0800

    Create Days15.java

---
+++ Days15.java
@@ -0,0 +1,10 @@
+public class main{
+  public static void main(String[] args) {
+    byte jumlah = 2;
+    jumlah += 3;
+    short kurang = 5;
+    kurang -= 3;
+    System.out.println("hasil jumlah 2 + 3: "+jumlah);
+    System.out.print("hasil kurang 5 - 3: "+kurang);
+   }
+}
