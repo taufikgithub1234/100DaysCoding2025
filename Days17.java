7e72dde53593e7d103e2bf54493a4dd7fce3d296
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-09-24 17:03:40 +0800

    Create Days17.java

---
+++ Days17.java
@@ -0,0 +1,8 @@
+public class modululs {
+    public static void main(String[] args) {     
+    short sisbag = 30;
+    byte pemgi = 4;
+        System.out.println("Sisa bagi dari 30 ke 4 adalah: "+(sisbag%pemgi));
+    }
+    
+}
