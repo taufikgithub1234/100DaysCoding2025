ca1dfe67809c01ab2350cbfe1f2142f1ff3cddd0
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-09-25 10:40:58 +0800

    Create Days18.java

---
+++ Days18.java
@@ -0,0 +1,18 @@
+public class main{
+  public static void main(String[] args){
+    byte a = 10;
+    short b = a;
+    int c = b;
+    long d = c;
+    float e = d;
+    double f = e;
+    System.out.println("Conversi dari nilai terkecil ke terbesar");
+    System.out.println("Nilai Byte: "+a);
+    System.out.println("Nilai short: "+b);
+    System.out.println("Nilai int: "+c);
+    System.out.println("Nilai Byte: "+d);
+    System.out.println("Nilai Byte: "+e);
+    System.out.println("Nilai Byte: "+f);
+    
+  }
+}
