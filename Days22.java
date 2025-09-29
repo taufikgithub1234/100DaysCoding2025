857d6608489b9a06e548b7125a60a565fabbee73
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-09-29 21:06:23 +0800

    Update Days22.java

--- Days22.java
+++ Days22.java
@@ -5,7 +5,7 @@ public class main{
     System.out.print("== Menghitung Luas Persegi ==");
     System.out.print("\nMasukkan nilai sisi: ");
     byte sisi = in.nextByte();
-    System.out.println(" Luas Persegi adalah nilai sisinya "+sisi+" adalah "+sisi*2);
+    System.out.println(" Luas Persegi adalah nilai sisinya "+sisi+" adalah "+sisi*sisi);
     
   }
 }
