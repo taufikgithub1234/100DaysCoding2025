7cae20b267f89e035c3fb0fc8f6f691f23a15e2f
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-09-30 21:17:12 +0800

    Update Days23.java

--- Days23.java
+++ Days23.java
@@ -6,9 +6,9 @@ public static void main(String[] args) {
   System.out.print("\nMasukkan nilai panjang: ");
   short p = in.nextShort();
 
-  System.out.print("\nMasukkan nilai Lebar ");
+  System.out.print("\nMasukkan nilai Lebar: ");
   short l = in.nextShort();
 
-  System.out.println(" jadi luas dari persegi panjang dengan nilai tersebut adalah: "+(p*l));
+  System.out.println("jadi luas dari persegi panjang dengan nilai tersebut adalah: "+(p*l));
 }
 }
