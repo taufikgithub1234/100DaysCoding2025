fbcdd865d1ee5b79079f400f642e1156f4252e33
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-09-30 21:15:04 +0800

    Create Days23.java

---
+++ Days23.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+  System.out.print("== Program Menghitung Luas Persegi Panjang ==");
+  System.out.print("\nMasukkan nilai panjang: ");
+  short p = in.nextShort();
+
+  System.out.print("\nMasukkan nilai Lebar ");
+  short l = in.nextShort();
+
+  System.out.println(" jadi luas dari persegi panjang dengan nilai tersebut adalah: "+(p*l));
+}
+}
