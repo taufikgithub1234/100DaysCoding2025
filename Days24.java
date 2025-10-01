0f94ac27df5a477326a20a80088272cb1072ac13
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-10-01 18:18:55 +0800

    Create Days24.java

---
+++ Days24.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+  public stativ void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.print("== Program membuat perhitungan luas lingkaran");
+    System.out.print("\n Diketahui phi: 3.14");
+    System.out.print("\n dan rumus: phi*r²");
+    System.out.print("dengan syarat r adalah kelipatan 7");
+    System.out.print("\n Masukkan nilai r: ");
+    int r = in.nextInt();
+    System.out.println("Luas lingkarang dari nilai r: "+r+" adalah: "+(3.14*r*r));
+    
+  }
+}
