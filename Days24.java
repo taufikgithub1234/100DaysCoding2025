f163a0fde17627a05bd463eee618eb15e601034f
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-10-01 18:21:03 +0800

    Update Days24.java

--- Days24.java
+++ Days24.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Main{
-  public stativ void main(String[] args) {
+  public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     System.out.print("== Program membuat perhitungan luas lingkaran");
     System.out.print("\n Diketahui phi: 3.14");
