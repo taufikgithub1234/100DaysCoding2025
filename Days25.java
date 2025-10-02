5f343fd9fc76d7c7bfd23896e6d76101cde8e811
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-10-02 20:52:09 +0800

    Create Days25.java

---
+++ Days25.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int angka = 5;
+
+        // Increment (tambah 1)
+        angka++;
+        System.out.println("Setelah increment: " + angka); 
+
+        // Decrement (kurang 1)
+        angka--;
+        System.out.println("Setelah decrement: " + angka);
+    }
+}
