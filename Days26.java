077b4605749f5204679c1917721445c483f5b315
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-10-03 19:10:04 +0800

    Create Days26.java

---
+++ Days26.java
@@ -0,0 +1,17 @@
+public class Main{
+  public static void main(String[] args) {
+    int a = 2;
+    a += 12;
+    System.out.println(a);
+    a -= 12;
+    System.out.println(a);
+    a /= 12;
+    System.out.println(a);
+    a *= 12;
+    System.out.println(a);
+    a %= 12;
+    System.out.println(a);
+    a = 12;
+    System.out.println(a);
+  }
+}
