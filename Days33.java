1c2f2515151a57e3792756f99f57ba84fb9bcc0c
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-10-11 00:18:40 +0800

    Create Days33.java

---
+++ Days33.java
@@ -0,0 +1,9 @@
+public class NotExample {
+    public static void main(String[] args) {
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println(!benar); // hasilnya: false
+        System.out.println(!salah); // hasilnya: true
+    }
+}
