2ad7b6acf6a707efc63360663d2495ed56e1f7f0
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-10-11 15:07:00 +0800

    Create Days35.java

---
+++ Days35.java
@@ -0,0 +1,10 @@
+public class Main{
+  public static void main(String[] args){
+    int a = 10;
+    if(a<=10){
+      System.out.println(a+" ini adalah bilangan dibawah/atau sama dengan 10");
+    }else{
+      System.out.println(a+" bukan dibawah atau sama dengan 10");
+    }
+  }
+}
