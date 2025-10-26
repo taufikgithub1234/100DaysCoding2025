49c8440de40787ab7f18487424c322743c0957ad
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-10-27 05:49:55 +0800

    Create Days49.java

---
+++ Days49.java
@@ -0,0 +1,11 @@
+public class Main{
+  public static void main(String [] args) {
+    int x = 10;
+int y = 5;
+
+String hasil = (x > y) ? "x lebih besar" : "y lebih besar atau sama";
+System.out.println(hasil);
+
+  }
+}
+
