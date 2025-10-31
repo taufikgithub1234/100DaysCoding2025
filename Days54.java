27a4eca98de23832dae9bb0ec7362de0450608fe
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-10-31 19:36:02 +0800

    Create Days54.java

---
+++ Days54.java
@@ -0,0 +1,11 @@
+public class Main{
+  public static void main(String[] args) {
+    for (int i = 1; i <= 4; i++) {
+    if (i == 3) {
+        continue;
+    }
+    System.out.println(i+" kenapa tidak ada 3 karena kamu satu satunya tak ada tiganya");
+System.out.println(":)");
+    }
+  }
+}
