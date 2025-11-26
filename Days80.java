81c90bb33cc1dfc02877ee64968f89ae59dd5330
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-11-26 19:07:47 +0800

    Create Days80.java

---
+++ Days80.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+	int angka[] = {2,4,4,3,4};
+	System.out.print("Index in Array: ");
+	for(int data : angka){
+	    System.out.print(data+" ");
+	}
+	}
+}
