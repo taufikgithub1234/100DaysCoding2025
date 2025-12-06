ed3bcba6bd50614c74bc83e476580b52b5db0926
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-12-06 20:14:53 +0800

    Create Days90.java

---
+++ Days90.java
@@ -0,0 +1,8 @@
+public class Main {		
+      public static void main(String[] args){
+        System.out.println("Luas persegi dengan sisi 4 = "+persegi(4));
+      }
+      public static int persegi(int a){
+         return a*a; 
+      }
+}
