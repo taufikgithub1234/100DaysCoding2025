a280cebf0f5698d39c3523e768706a6cc34b6356
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-12-07 19:02:39 +0800

    Create Days91.java

---
+++ Days91.java
@@ -0,0 +1,11 @@
+public class Main{
+
+public static void main(String[] args){
+
+param();
+}
+static void param() {
+System.out.println("ini adalah contoh parameter method -> ()");
+System.out.println("static void param()");
+}
+}
