262cc1469128804a19e42f8b37ca3e859950d698
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-12-10 22:08:06 +0800

    Create Days94.java

---
+++ Days94.java
@@ -0,0 +1,8 @@
+public class Main {		
+      public static void main(String[] args){
+        Java("Java");
+      }
+      static void Java(String nama){
+         System.out.println("Hai saya "+nama);
+          }
+}
