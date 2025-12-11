e3627be54bf9066207aae1fb66c58955f7ea5a79
taufikgithub1234 <155694581+taufikgithub1234@users.noreply.github.com>
2025-12-11 20:20:04 +0800

    Create Days95.java

---
+++ Days95.java
@@ -0,0 +1,8 @@
+public class Main {		
+      public static void main(String[] args){
+        Java("Adalah bahasa java");
+      }
+      static void Java(String nama){
+         System.out.println("Hai saya "+nama);
+          }
+}
